f152f14361fcaa92730107938840366f5439f2e4
hotpucko <stn.jonsson@gmail.com>
2020-02-07 13:29:40 +0100

    Cleaned up and made Run.Java use CBR Node

--- src/Sim/Run.java
+++ src/Sim/Run.java
@@ -1,43 +1,27 @@
 package Sim;
 
 import java.util.Arrays;
-import java.util.Comparator;
-
-// An example of how to build a topology and starting the simulation engine
 
 public class Run {
 	public static void main (String [] args)
 	{
- 		//Creates two links
  		LossyLink link1 = new LossyLink(25, 10, 0.25);
  		LossyLink link2 = new LossyLink(0, 0, 0);
 		
-		// Create two end hosts that will be
-		// communicating via the router
-		Node host1 = new Node(1,1);
-		Node host2 = new Node(2,1);
+		Node host1 = new Sink(1,1);
+		CBR host2 = new CBR(2,1);
 
-		//Connect links to hosts
-		host1.setPeer(link1);
+		host1.setPeer(link2);
 		host2.setPeer(link2);
-
-		// Creates as router and connect
-		// links to it. Information about 
-		// the host connected to the other
-		// side of the link is also provided
+		
 		// Note. A switch is created in same way using the Switch class
 		Router routeNode = new Router(2);
-		routeNode.connectInterface(0, link1, host1);
+		routeNode.connectInterface(0, link2, host1);
 		routeNode.connectInterface(1, link2, host2);
-		int packetsSentHostOne = 10;
-		int packetsSentHostTwo = 0;
-		// Generate some traffic
-		// host1 will send 3 messages with time interval 5 to network 2, node 1. Sequence starts with number 1
-		host1.StartSending(2, 2, packetsSentHostOne, 5, 1); 
-		// host2 will send 2 messages with time interval 10 to network 1, node 1. Sequence starts with number 10
-		host2.StartSending(1, 1, packetsSentHostTwo, 10, 101); 
+
+		//Network, Node, Messages per second, Seconds to send
+		host2.StartSending(1, 1, 2, 10);
 		
-		// Start the simulation engine and of we go!
 		Thread t=new Thread(SimEngine.instance());
 	
 		t.start();
