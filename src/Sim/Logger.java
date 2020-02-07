9636c9cae2a3ec71dc5227b764cb76a9fe328f21
hotpucko <stn.jonsson@gmail.com>
2020-02-07 12:59:38 +0100

    Added comment and cleaned up

--- src/Sim/Logger.java
+++ src/Sim/Logger.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 
 public class Logger {
 
+	public static String newLine = System.getProperty("line.separator");
+	
 	public static void LogTime(String fileToWrite, String data) throws IOException
 	{
-			BufferedWriter writer = new BufferedWriter(new FileWriter(fileToWrite, true));
-			writer.write(data + "\n");
-			writer.close();
+		
+		//IOException: if the named file exists but is a directory rather than a regular file, does not exist but cannot be created, or cannot be opened for any other reason
+		BufferedWriter writer = new BufferedWriter(new FileWriter(fileToWrite, true));
+		writer.write(data + newLine);
+		writer.close();
 	}
-	
 }
